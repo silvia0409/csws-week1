e34d3c3fb5a538dfa7cbd6510ad3655d3a5a81a1
Silvia Nwakamma <nwakamma2004@gmail.com>
2024-01-16 11:57:24 +0000

    multiplication of numbers in list

--- week1.py
+++ week1.py
@@ -1,1 +1,12 @@
 print("Hello world")
+numbers=[2,1,4]
+
+for i in range (6):
+    print("This is my first program")
+
+def calculate(numbers):
+        multiplication=numbers[0]*numbers[1]*numbers[2]
+        print("The multiplication equals " ,multiplication)
+    
+    
+calculate(numbers)
